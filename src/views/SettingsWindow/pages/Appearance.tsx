71b6d16bf7ca0c0c72e38ae923ef23976f25608a
Michael Polowczuk <michaelp@solisware.com>
2026-05-29 01:00:02 +0100

    Designed a basic app theme switch

--- src/views/SettingsWindow/pages/Appearance.tsx
+++ src/views/SettingsWindow/pages/Appearance.tsx
@@ -4,8 +4,39 @@
  * All rights reserved. Licensed under the MIT license.
  * See the LICENSE.txt file in the project root directory for details.
  */
+import styles from "./SettingsPages.module.css";
+
 function Appearance() {
-  return <></>;
+  return (
+    <div className={styles.appearancePage}>
+      <header className={styles.linksHeader}>
+        <h1 className={styles.pageTitle}>Appearance</h1>
+      </header>
+      <section className={styles.settingsSection} aria-labelledby="appearance-theme-title">
+        <div className={styles.themeRow}>
+          <h3 className={styles.settingsSectionTitle} id="appearance-theme-title">Application Theme</h3>
+          <fieldset className={styles.radioGroup}>
+            <legend className={styles.visuallyHidden}>Application Theme</legend>
+            <label className={styles.radioOption}>
+              <input className={styles.radioInput} type="radio" name="app-theme" value="auto" defaultChecked />
+              <span className={styles.radioLabel}>Auto</span>
+              <span className={styles.radioControl} aria-hidden="true" />
+            </label>
+            <label className={styles.radioOption}>
+              <input className={styles.radioInput} type="radio" name="app-theme" value="light" />
+              <span className={styles.radioLabel}>Light</span>
+              <span className={styles.radioControl} aria-hidden="true" />
+            </label>
+            <label className={styles.radioOption}>
+              <input className={styles.radioInput} type="radio" name="app-theme" value="dark" />
+              <span className={styles.radioLabel}>Dark</span>
+              <span className={styles.radioControl} aria-hidden="true" />
+            </label>
+          </fieldset>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Appearance;
